937c07b40e3b519f4812fb86f8c4b35635cc98b0
kai-city <1830170041@qq.com>
2020-07-27 20:37:07 +0800

    新建工程

---
+++ app/src/main/java/cn/kaicity/app/iguangke/MainActivity.kt
@@ -0,0 +1,12 @@
+package cn.kaicity.app.iguangke
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
