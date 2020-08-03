c066852de0005d84d4deb22326cd47d617b54d20
kai-city <1830170041@qq.com>
2020-08-03 23:53:42 +0800

    集成友盟统计

--- app/src/main/java/cn/kaicity/app/iguangke/App.kt
+++ app/src/main/java/cn/kaicity/app/iguangke/App.kt
@@ -2,14 +2,20 @@ package cn.kaicity.app.iguangke
 
 import android.app.Application
 import android.content.Context
+import com.umeng.commonsdk.UMConfigure
 
 class App : Application() {
 
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
-        App.context=base!!
+        context =base!!
     }
 
+
+    override fun onCreate() {
+        super.onCreate()
+        UMConfigure.init(this, "59892f08310c9307b60023d0", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, "")
+    }
     companion object{
         lateinit var context: Context
     }
